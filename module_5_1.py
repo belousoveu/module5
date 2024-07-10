4426779af423652e6849c4cf792a7697456e46c3
Belousoveu <belousov.eu@gmail.com>
2024-07-10 10:20:17 +0300

    module_5_2 complete

--- module_5_1.py
+++ module_5_1.py
@@ -5,6 +5,29 @@ class House:
         self.number_of_floors = number_of_floors
         self.current_floor = 1
 
+    def __str__(self):
+        return f'{self.name} имеет {self.number_of_floors} этажей'
+
+    def __hash__(self):
+        return hash(self.name, str(self.number_of_floors))
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Ошибка. Такого этажа не существует')
+            return
+        else:
+            self.current_floor = new_floor
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Ошибка. Такого этажа не существует')
+            return
+        else:
+            self.current_floor = new_floor
+
     def go_to(self, new_floor):
         if new_floor < 1 or new_floor > self.number_of_floors:
             print('Ошибка. Такого этажа не существует')
@@ -45,4 +68,15 @@ house_1.lift(17)
 house_1.lift(12)
 house_1.lift(12)
 house_1.lift(25)
+
+house_3 = House('ЖК Эльбрус', 10)
+house_4 = House('ЖК Акация', 20)
+print(house_1)
+print(len(house_1))
+print(house_2)
+print(len(house_2))
+print(house_3)
+print(len(house_3))
+print(house_4)
+print(len(house_4))
 
